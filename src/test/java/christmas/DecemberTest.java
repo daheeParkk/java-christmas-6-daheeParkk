1ac44926d2d69bd0611eec31580ec754be12069e
daheeParkk <qkrekgml7414@naver.com>
2023-11-13 21:28:25 +0900

    test : 날짜가 일요일이면 true를 반환하는 테스트 작성

--- src/test/java/christmas/DecemberTest.java
+++ src/test/java/christmas/DecemberTest.java
@@ -1,13 +1,19 @@
 package christmas;
 
+import christmas.domain.December;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class DateTest {
+public class DecemberTest {
     
     @DisplayName("날짜가 일요일이면 true를 반환한다.")
     @ParameterizedTest
-    public void isSundayTest() {
-    
+    @ValueSource(ints = {3, 10, 17, 24, 31})
+    public void isSundayTest(int day) {
+        December date = new December(day);
+        
+        Assertions.assertTrue(date.isSunday());
     }
 }
