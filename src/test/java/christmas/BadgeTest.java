9e55a0b1551b16643efb461b0b989dbf4dc9bd59
daheeParkk <qkrekgml7414@naver.com>
2023-11-13 22:35:47 +0900

    test : 금액이 5,000원 아래면 null이 반환되는 테스트 작성

--- src/test/java/christmas/BadgeTest.java
+++ src/test/java/christmas/BadgeTest.java
@@ -1,2 +1,19 @@
-package christmas;public class BadgeTest {
+package christmas;
+
+import christmas.domain.Badge;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BadgeTest {
+    
+    @DisplayName("금액이 5,000원 아래면 null이 반환된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 4000, 4999})
+    public void createNullBadgeTest(int purchaseAmount) {
+        Badge badge = Badge.create(purchaseAmount);
+    
+        Assertions.assertThat(badge).isEqualTo(null);
+    }
 }
