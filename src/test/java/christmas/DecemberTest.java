51f15f510242d37444efea63d851671b268c71a9
daheeParkk <qkrekgml7414@naver.com>
2023-11-13 21:35:42 +0900

    test : 날짜가 크리스마스라면 true를 반환하고 아니면 false를 반환하는 테스트 작성

--- src/test/java/christmas/DecemberTest.java
+++ src/test/java/christmas/DecemberTest.java
@@ -1,9 +1,12 @@
 package christmas;
 
+import static org.assertj.core.api.Assertions.*;
+
 import christmas.domain.December;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class DecemberTest {
@@ -24,5 +27,14 @@ public class DecemberTest {
         December date = new December(day);
         
         Assertions.assertFalse(date.isSunday());
+    }
+    
+    @DisplayName("날짜가 크리스마스라면 true를 반환하고 아니면 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"25-true","24-false","26-false"}, delimiter = '-')
+    public void isChristmasTest(int day, boolean result) {
+        December date = new December(day);
+    
+        assertThat(date.isChristmas()).isEqualTo(result);
     }
 }
