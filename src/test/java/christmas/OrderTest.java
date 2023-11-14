234e12b9f1db7396068717d9f7f8e653829494bc
daheeParkk <qkrekgml7414@naver.com>
2023-11-15 00:29:46 +0900

    test : 특정 메뉴 타입의 개수를 세는 기능 테스트 작성

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -1,2 +1,21 @@
-package christmas;public class OrderTest {
+package christmas;
+
+import christmas.domain.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class OrderTest {
+    
+    @DisplayName("메뉴 타입인 메인 메뉴를 전달하면 메인 메뉴의 개수를 센다.")
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1=2", "크리스마스파스타-2,바비큐립-3,초코케이크-2=5"}, delimiter = '=')
+    public void countMenuTypeTest(String menu, int result) {
+        Order order = new Order(menu);
+        
+        int menuCount = order.countMenuType("메인");
+        
+        Assertions.assertThat(menuCount).isEqualTo(result);
+    }
 }
