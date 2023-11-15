30df1d7b71951a233cd0eb2b30f1187c08816ad0
daheeParkk <qkrekgml7414@naver.com>
2023-11-15 18:42:16 +0900

    feat : Controller 작성

---
+++ src/main/java/christmas/controller/WooTeCoRestaurantController.java
@@ -0,0 +1,71 @@
+package christmas.controller;
+
+import christmas.service.ReservationService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class WooTeCoRestaurantController {
+    
+    private static final OutputView outputView = new OutputView();
+    private static final InputView inputView = new InputView();
+    private static final ReservationService reservationService = new ReservationService();
+    
+    public void operate() {
+        entertainCustomer();
+        confirmOrder();
+        checkBenefits();
+    }
+    
+    private void checkBenefits() {
+        checkGiveawayMenu();
+        checkDiscount();
+        checkTotalDiscountAmount();
+        checkAmountOfPayment();
+        checkBadge();
+    }
+    
+    private void checkGiveawayMenu() {
+        outputView.outputGiveawayMenuHeader();
+        outputView.outputGiveawayMenu(reservationService.checkGiveawayMenu());
+    }
+    
+    private void checkDiscount() {
+        outputView.outputBenefitsHeader();
+        outputView.outputBenefits(reservationService.checkDiscount());
+    }
+    
+    private void checkTotalDiscountAmount() {
+        outputView.outputTotalBenefitAmountHeader();
+        outputView.outputTotalBenefitAmount(reservationService.checkTotalDiscountAmount());
+    }
+    
+    private void checkAmountOfPayment() {
+        outputView.outputAmountOfPaymentHeader();
+        outputView.outputAmountOfPayment(reservationService.checkAmountOfPayment());
+    }
+    
+    private void checkBadge() {
+        outputView.outputBadgeHeader();
+        outputView.outputBadge(reservationService.checkBadge());
+    }
+    
+    private void confirmOrder() {
+        outputView.outputBenefitPreview();
+        
+        outputView.outputOrderMenuHeader();
+        outputView.outputOrderMenu(reservationService.checkOrderInformation());
+        
+        outputView.outputTotalOrderAmountHeader();
+        outputView.outputTotalOrderAmount(reservationService.checkTotalOrderAmount());
+    }
+    
+    private void entertainCustomer() {
+        outputView.outputGreetings();
+        
+        int day = Integer.parseInt(inputView.inputDateOfVisit());
+        reservationService.reserveDate(day);
+        
+        String menu = inputView.inputOrderMenu();
+        reservationService.orderMenu(menu);
+    }
+}
---
+++ src/main/java/christmas/service/ReservationService.java
@@ -0,0 +1,42 @@
+package christmas.service;
+
+import java.util.Map;
+
+public class ReservationService {
+    
+    public void reserveDate(int day) {
+    
+    }
+    
+    public void orderMenu(String menu) {
+    
+    }
+    
+    public Map<String, Integer> checkOrderInformation() {
+        return null;
+    }
+    
+    public int checkTotalOrderAmount() {
+        return 0;
+    }
+    
+    public Map<String, Integer> checkGiveawayMenu() {
+        return null;
+    }
+    
+    public Map<String, Integer> checkDiscount() {
+        return null;
+    }
+    
+    public int checkTotalDiscountAmount() {
+        return 0;
+    }
+    
+    public int checkAmountOfPayment() {
+        return 0;
+    }
+    
+    public String checkBadge() {
+        return null;
+    }
+}
